0ae59aad6222a78dab0dd51d42dd3b785e054f62
Anna <anna.spiegelmayr@gmx.at>
2018-05-09 16:59:50 +0200

    I added a method yay

--- Spiegelmayr_Katzinger_android/aj/src/snippet/Snippet.java
+++ Spiegelmayr_Katzinger_android/aj/src/snippet/Snippet.java
@@ -3,6 +3,9 @@ package snippet;
 public class Snippet {
 	public static void main(String[] args) {
 		C:\Users\Teufe\Tools2018SpiegelmayrKatzinger
+        public void thisIsANewMethod() {
+            System.out.print ("Hello, I am a method.");
+        }
 	}
 }
 
