66dc4bf0942c28b3e8620b815b3f2f7ad748f9ef
Philip1234567890 <S1610237013@students.fh-hagenberg.at>
2018-05-09 20:45:26 +0200

    d

--- Spiegelmayr_Katzinger_android/aj/src/snippet/Snippet.java
+++ Spiegelmayr_Katzinger_android/aj/src/snippet/Snippet.java
@@ -2,7 +2,7 @@ package snippet;
 
 public class Snippet {
 	public static void main(String[] args) {
-		C:\Users\Teufe\Tools2018SpiegelmayrKatzinger
+
         public void thisIsANewMethod() {
             System.out.print ("Hello, I am a method.");
             System.out.print("I am a smart method and I love cake.");
