666fc966f6ca7db0fb985cd6459524fffa9dbffa
Anna <anna.spiegelmayr@gmx.at>
2018-05-09 17:04:30 +0200

    Method is wonderful

--- Spiegelmayr_Katzinger_android/aj/src/snippet/Snippet.java
+++ Spiegelmayr_Katzinger_android/aj/src/snippet/Snippet.java
@@ -5,6 +5,7 @@ public class Snippet {
 		C:\Users\Teufe\Tools2018SpiegelmayrKatzinger
         public void thisIsANewMethod() {
             System.out.print ("Hello, I am a method.");
+            System.out.print("I am a smart method and I love cake.");
         }
 	}
 }
