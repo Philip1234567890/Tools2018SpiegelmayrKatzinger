8b057559515da87cc9f109a4a7e72da02db23318
Philip1234567890 <S1610237013@students.fh-hagenberg.at>
2018-05-09 20:49:15 +0200

    FIXED

--- Spiegelmayr_Katzinger_android/aj/src/snippet/Snippet.java
+++ Spiegelmayr_Katzinger_android/aj/src/snippet/Snippet.java
@@ -2,12 +2,12 @@ package snippet;
 
 public class Snippet {
 	public static void main(String[] args) {
-		C:\Users\Teufe\Tools2018SpiegelmayrKatzinger
+
         public void thisIsANewMethod() {
             System.out.print ("Hello, I am a method.");
             System.out.print("I am a smart method and I love cake.");
         }
 System.out.println("You got git gudded, please call mr kurz");
-	}
+	}//JUHU BUG FIXED 
 }
 
